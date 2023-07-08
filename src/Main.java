687be34d6daeeb188e2755661f3c7f9ee30a1846
MasimovOleg <qfqo9h!@#>
2023-07-08 22:25:07 +0300

    ValidMountainArray

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import arrays.RemoveDuplicatesFromSortedArray;
 import arrays.ValidMountainArray;
 import common.ReverseLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ValidMountainArray.main());
+        System.out.println();
 
     }
 }
---
+++ src/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        int[] out = new int[nums.length];
+        int indexOut = 0;
+        out[indexOut] = nums[indexOut];
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] != nums[i+1]) {
+                out[++indexOut] = nums[i+1];
+            }
+        }
+        System.out.println(indexOut+1);
+    }
+}
