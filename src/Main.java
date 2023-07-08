2472fecb43c19f049a612bd05f619b597d79de36
MasimovOleg <qfqo9h!@#>
2023-07-08 21:51:15 +0300

    ValidMountainArray

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import arrays.ValidMountainArray;
 import common.ReverseLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ReverseLinkedList.main());
+        System.out.println(ValidMountainArray.main());
 
     }
 }
--- src/arrays/ValidMountainArray.java
+++ src/arrays/ValidMountainArray.java
@@ -1,4 +1,30 @@
 package arrays;
 
 public class ValidMountainArray {
+    public static boolean main() {
+        int[] arr = {9,8,7,6,5,4,3,2,1,0};
+
+        boolean isInc = true;
+        boolean result = false;
+        if (arr.length == 2) {
+            return false;
+        }
+        for (int i = 0; i < arr.length-1; i++) {
+            if(isInc) {
+                if (arr[i]>=arr[i + 1]) {
+                    isInc = false;
+                } else {
+                    result = true;
+                }
+
+            } else {
+                if (arr[i]<=arr[i + 1]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        if (arr[arr.length-1]>= arr[arr.length-2]) result = false;
+        return result;
+    }
 }
